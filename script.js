f7c46f8f19e7fa98b0d47901fecd7f9377a4974c
Rahul Varma <rahulvarma300@gmail.com>
2023-10-19 22:24:48 +0530

    Syntax

---
+++ script.js
@@ -0,0 +1,16 @@
+/*Syntax:
+Arrow functions have a shorter syntax compared to normal functions.*/
+
+// Normal function
+
+function func(e){
+    console.log(e)
+}
+
+func(5);
+
+//Arrow FUnction
+
+const Func = (e) => {console.log(e);}
+
+Func(6)
