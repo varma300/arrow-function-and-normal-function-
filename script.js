e399c61c8234748085ed9fc241d32dd5466325ce
Rahul Varma <rahulvarma300@gmail.com>
2023-10-19 22:28:34 +0530

    this Binding

--- script.js
+++ script.js
@@ -14,3 +14,33 @@ func(5);
 const Func = (e) => {console.log(e);}
 
 Func(6)
+
+/*this Binding:
+Arrow functions do not have their own this binding. Instead, they inherit the this value from their enclosing (parent) function or scope.
+Normal functions have their own this binding, which is determined by how they are called.this Binding:
+Arrow functions do not have their own this binding. Instead, they inherit the this value from their enclosing (parent) function or scope.
+Normal functions have their own this binding, which is determined by how they are called.*/
+
+//Normal Function:
+
+const person = {
+    name: 'John',
+    sayHello: function() {
+      console.log(`Hello, ${this.name}`); // 'this' refers to the 'person' object
+    },
+  };
+  
+  person.sayHello(); // Output: Hello, John
+
+  
+  //Arrow Function:
+
+  const persons = {
+    name: 'John',
+    sayHello: () => {
+      console.log(`Hello, ${this.name}`); // 'this' refers to the global scope, not 'person'
+    },
+  };
+  
+  person.sayHello(); // Output: Hello, undefined
+  
